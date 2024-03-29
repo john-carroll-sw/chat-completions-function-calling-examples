5960c9b6beb1ebdf1d59b42e9062e41859398b51
john-carroll-sw <johncornellcarroll@gmail.com>
2024-03-29 11:45:31 -0400

    added an of streaming + function calling using the get weather function call

--- weather_example_streaming.py
+++ weather_example_streaming.py
@@ -1,6 +1,7 @@
 import os
 from openai import AzureOpenAI
 import json
+import asyncio
 
 client = AzureOpenAI(
     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
@@ -68,60 +69,91 @@ def run_conversation():
             },
         }
     ]
-    response = client.chat.completions.create(
+    stream = client.chat.completions.create(
         model=os.getenv("AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"),
         messages=messages,
         tools=tools,
         tool_choice="auto",  # auto is default, but we'll be explicit
         temperature=0,  # Adjust the variance by changing the temperature value (default is 0.8)
+        stream=True
     )
 
-    response_message = response.choices[0].message
-    tool_calls = response_message.tool_calls
+    available_functions = {
+        "get_current_weather": get_current_weather,
+    }  # only one function in this example, but you can have multiple
+    
+    tool_calls = get_tool_calls(stream)
 
     # Step 2: check if the model wanted to call a function
     if tool_calls:
+        messages.append(
+            {
+                "tool_calls": tool_calls,
+                "role": 'assistant',
+            }                    
+        )
 
-        messages.append(response_message)  # extend conversation with assistant's reply
-        available_functions = {
-            "get_current_weather": get_current_weather,
-        }  # only one function in this example, but you can have multiple
-        
         for tool_call in tool_calls:
 
             # Note: the JSON response may not always be valid; be sure to handle errors
-            function_name = tool_call.function.name
+            function_name = tool_call['function']['name']
             if function_name not in available_functions:
                 return "Function " + function_name + " does not exist"
         
             # Step 3: call the function with arguments if any
             function_to_call = available_functions[function_name]
-            function_args = json.loads(tool_call.function.arguments)
+            function_args = json.loads(tool_call['function']['arguments'])
             function_response = function_to_call(**function_args)
 
             # Step 4: send the info for each function call and function response to the model
             messages.append(
                 {
-                    "tool_call_id": tool_call.id,
+                    "tool_call_id": tool_call['id'],
                     "role": "tool",
                     "name": function_name,
                     "content": function_response,
                 }
             )  # extend conversation with function response
 
-        second_response = client.chat.completions.create(
+        stream = client.chat.completions.create(
             model=os.getenv("AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"),
             messages=messages,
-        )  # get a new response from the model where it can see the function response
-        return second_response
-    
+            stream=True,
+        )
 
-# print(run_conversation())
+        async def print_stream_chunks(stream):
+            for chunk in stream:
+                if chunk.choices and chunk.choices[0].delta.content is not None:
+                    print(chunk.choices[0].delta.content, end="", flush=True)
+                    await asyncio.sleep(0.1)
+
+        asyncio.run(print_stream_chunks(stream))
+
+def get_tool_calls(stream):
+    tool_calls = []
+    delta = None
+
+    for chunk in stream:
+        delta = chunk.choices[0].delta if chunk.choices and chunk.choices[0].delta is not None else None
+        # print(delta)
+
+        if delta and delta.content:
+            print(delta.content)
+
+        elif delta and delta.tool_calls:
+            tc_chunk_list = delta.tool_calls
+            for tc_chunk in tc_chunk_list:
+                if len(tool_calls) <= tc_chunk.index:
+                    tool_calls.append({"id": "", "type": "function", "function": {"name": "", "arguments": ""}})
+                tc = tool_calls[tc_chunk.index]
+
+                if tc_chunk.id:
+                    tc["id"] += tc_chunk.id
+                if tc_chunk.function.name:
+                    tc["function"]["name"] += tc_chunk.function.name
+                if tc_chunk.function.arguments:
+                    tc["function"]["arguments"] += tc_chunk.function.arguments
+    return tool_calls
+
 result = run_conversation()
-
-# from pprint import pprint
-# pprint(vars(result))
-
-message_content = result.choices[0].message.content
-print(message_content)
 
